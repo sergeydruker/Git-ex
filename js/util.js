a518e19ee2f1ffebb576ed636ace3c49caf635dd
sergeydruker <sergeydruker@gmail.com>
2026-08-20 12:16:25 +0300

    add util page and rng func

---
+++ js/util.js
@@ -0,0 +1,7 @@
+'use strict'
+
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
